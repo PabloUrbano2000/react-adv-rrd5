e01335e7f12ea4c7ea13e968c9cb55fa0e7c617c
Pablo El Maldy <pablojamiro2008@hotmail.com>
2024-08-23 10:38:11 -0500

    lazy lad

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,47 @@
+import {
+  NavLink,
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from 'react-router-dom'
+
+import { Suspense } from 'react'
+import logo from '../logo.svg'
+import { routes } from './routes'
+
+export const Navigation = () => {
+  return (
+    <Suspense fallback={<span>Loading</span>}>
+      <Router>
+        <div className='main-layout'>
+          <nav>
+            <img src={logo} alt='React Logo' />
+            <ul>
+              {routes.map((route) => (
+                <li key={route.name}>
+                  <NavLink to={route.path} activeClassName='nav-active' exact>
+                    {route.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                render={() => <route.Component />}
+              />
+            ))}
+            <Redirect to={routes[0].path} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
+  )
+}
